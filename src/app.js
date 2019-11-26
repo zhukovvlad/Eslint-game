83dbc686aac1597d272dc70e98cdac8b986389e7
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-11-26 16:04:04 +0300

    Fixed Eslint errors

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const characters = [
   { name: 'маг', health: 0 },
   { name: 'лучник', health: 0 },
 ];
-const alive = characters.filter(item => item.health > 0);
+const alive = characters.filter((item) => item.health > 0);
